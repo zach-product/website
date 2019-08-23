228e2f33ef4795f438b2624ddea36fa4ca46082a
prodforward <zach@prodforward.com>
2019-08-23 13:02:53 +0200

    Added NavBar

---
+++ src/components/Logo.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import logo from '../logo.svg'
+
+const Wrapper = styled.a.attrs({
+    className: 'navbar-brand',
+})``
+
+class Logo extends Component {
+    render() {
+        return (
+            <Wrapper href="https://prodforward.com">
+                <img src={logo} width="50" height="50" alt="Prod Forward Logo" />
+            </Wrapper>
+        )
+    }
+}
+
+export default Logo
